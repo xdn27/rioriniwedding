10d106cbf9ba97a13e927a14b911d3da8be4d4c5
Denisa Rachman <27.idea@gmail.com>
2020-10-31 14:14:08 +0700

    600

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ let isAnimate = false;
 
 new fullpage('#fullpage', {
     autoScrolling: true,
-    responsiveHeight: 620,
+    responsiveHeight: 600,
     afterSlideLoad: function(origin, destination){
         $('.slide.active [data-aos]').addClass("aos-animate");
     },
